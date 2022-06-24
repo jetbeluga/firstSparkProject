b8205ed78395f4e2fd046af1a8cc7f24069bdf8c
Moritz Piehl <moritz.piehl@bluewin.ch>
2022-06-24 18:46:52 +0200

    added red luminous effect

--- first_spark_project/scripts/script.js
+++ first_spark_project/scripts/script.js
@@ -2,6 +2,8 @@
  * (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
  */
 
+import { pack4 } from "Reactive";
+
 //import { mod } from "Reactive";
 
 //==============================================================================
@@ -24,6 +26,9 @@ const Scene = require("Scene");
 export const Diagnostics = require("Diagnostics");
 const Time = require("Time");
 const Reactive = require("Reactive");
+const Materials = require("Materials");
+const Shaders = require("Shaders");
+const Textures = require("Textures");
 
 // To use variables and functions across files, use export/import keyword
 // export const animationDuration = 10;
@@ -32,38 +37,34 @@ const Reactive = require("Reactive");
 // import { animationDuration } from './script.js'
 
 (async function () {
-  // Enables async/await in JS [part 1]
-  const Materials = require("Materials");
-  const Shaders = require("Shaders");
-  const Textures = require("Textures");
   const [cameraTexture, material0] = await Promise.all([
     Textures.findFirst("cameraTexture0"),
     Materials.findFirst("material0"),
   ]);
 
+  function luminance(color) {
+    return Reactive.dot(color, Reactive.pack4(0.2125, 0.7154, 0.721, 0));
+  }
+
+  function modulate(time, lum) {
+    const ct = Reactive.mul(time, 0.001);
+    const curve = Reactive.abs(Reactive.sin(ct));
+    const modulationColor = Reactive.pack4(curve, 0, 0, 1);
+    const lum4 = Reactive.pack4(lum, lum, lum, 1);
+    const finalModulation = Reactive.add(lum4, modulationColor);
+    return finalModulation;
+  }
+
   function main() {
     const uvs = Shaders.vertexAttribute({
       variableName: Shaders.VertexAttribute.TEX_COORDS,
     });
-    const ct = Reactive.mul(Time.ms, 0.001);
-    const curve = Reactive.abs(Reactive.sin(ct));
-
     const color = Shaders.textureSampler(cameraTexture.signal, uvs);
-    const modulationColor = Reactive.pack4(curve, 0, 0, 1);
-    //Diagnostics.watch("curve", curve);
+    const lum = luminance(color);
+    const modulationColor = modulate(Time.ms, lum);
     const finalColor = Reactive.mul(color, modulationColor);
     const textureSlot = Shaders.DefaultMaterialTextures.DIFFUSE;
     material0.setTextureSlot(textureSlot, finalColor);
   }
   main();
-  // To access scene objects
-  // const [directionalLight] = await Promise.all([
-  //   Scene.root.findFirst('directionalLight0')
-  // ]);
-
-  // To access class properties
-  // const directionalLightIntensity = directionalLight.intensity;
-
-  // To log messages to the console
-  // Diagnostics.log('Console message logged from the script.');
-})(); // Enables async/await in JS [part 2]
+})();
